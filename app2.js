1d63a076dc117c2da60b0888a970d1b5078cf025
rhariprasadreddy <hari_2004@yahoo.com>
2020-05-03 14:00:32 +0800

    Hero Feature to Branch

--- app2.js
+++ app2.js
@@ -1,1 +1,3 @@
+//To Do Hero Feature addition to App2 we have amended the code
 console.log("Hello Welcome Rohith -- Mummy fighting World!\n");
+console.log("Hello App2 we have added Boost to you \n");
